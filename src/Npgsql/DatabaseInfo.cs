8202858848d12ea44438207ff1f2c2af4647f5ec
apircher <apircher@luxbox.net>
2017-07-24 17:08:26 +0200

    Adapted type metadata queries for CrateDB.

--- src/Npgsql/DatabaseInfo.cs
+++ src/Npgsql/DatabaseInfo.cs
@@ -102,7 +102,61 @@ WHERE
 ORDER BY ord";
         }
 
+        static readonly string CrateTypesQueryForBaseTypes = GenerateCrateTypesQuery(true);
+        static readonly string CrateTypesQueryForArrayTypes = GenerateCrateTypesQuery(false);
+
+        static string GenerateCrateTypesQuery(bool forBaseTypes)
+        {
+            return
+$@"select 
+    'pg_catalog' as nspname, typname, oid, 0 as typrelid, 0 as typbasetype, 
+    '{(forBaseTypes ? "b" : "a")}' as type, 
+    {(forBaseTypes ? "0" : "typelem")} as elemoid
+from
+    pg_catalog.pg_type
+where
+    {(forBaseTypes ? "" : "not ")}typelem = 0";
+        }
+
         async Task LoadBackendTypes(NpgsqlConnector connector, NpgsqlTimeout timeout, bool async)
+        {
+            if (connector.IsCrateDB)
+            {
+                await LoadCrateDbBackendTypes(connector, timeout, async);
+            }
+            else
+            {
+                var commandTimeout = 0;  // Default to infinity
+                if (timeout.IsSet)
+                {
+                    commandTimeout = (int)timeout.TimeLeft.TotalSeconds;
+                    if (commandTimeout <= 0)
+                        throw new TimeoutException();
+                }
+
+                using (var command = new NpgsqlCommand(connector.SupportsRangeTypes ? TypesQueryWithRange : TypesQueryWithoutRange, connector.Connection))
+                {
+                    command.CommandTimeout = commandTimeout;
+                    command.AllResultTypesAreUnknown = true;
+                    using (var reader = async ? await command.ExecuteReaderAsync() : command.ExecuteReader())
+                    {
+                        while (async ? await reader.ReadAsync() : reader.Read())
+                        {
+                            timeout.Check();
+                            LoadBackendType(reader, connector);
+                        }
+                    }
+                }
+            }
+        }
+
+        async Task LoadCrateDbBackendTypes(NpgsqlConnector connector, NpgsqlTimeout timeout, bool async)
+        {
+            await LoadBackendTypes(connector, timeout, async, CrateTypesQueryForBaseTypes);
+            await LoadBackendTypes(connector, timeout, async, CrateTypesQueryForArrayTypes);
+        }
+
+        async Task LoadBackendTypes(NpgsqlConnector connector, NpgsqlTimeout timeout, bool async, string typesQuery)
         {
             var commandTimeout = 0;  // Default to infinity
             if (timeout.IsSet)
@@ -112,7 +166,7 @@ ORDER BY ord";
                     throw new TimeoutException();
             }
 
-            using (var command = new NpgsqlCommand(connector.SupportsRangeTypes ? TypesQueryWithRange : TypesQueryWithoutRange, connector.Connection))
+            using (var command = new NpgsqlCommand(typesQuery, connector.Connection))
             {
                 command.CommandTimeout = commandTimeout;
                 command.AllResultTypesAreUnknown = true;
